9a7943ddd169de629c615ce857171e02afbab3dd
AkshitaAg25 <akshita.agarwal_cs.aiml24@gla.ac.in>
2026-02-19 20:00:36 +0530

    set and pin intialisation

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author AkshitaAg25
+ * @date 2026-02-19
  *
  * @details
  * Controls RGB LED using digital ON/OFF
@@ -12,23 +12,31 @@
 
  // TODO 1:
  // Define RED pin (Use 9)
+int red=9;
 
  // TODO 2:
  // Define GREEN pin (Use 10)
+int green=10;
 
  // TODO 3:
  // Define BLUE pin (Use 11)
+int blue=11;
 
 void setup() {
 
     // TODO 4:
     // Initialize Serial communication (9600 baud)
+    Serial.begin(9600);
 
     // TODO 5:
     // Configure RGB pins as OUTPUT
+    pinMode(red,OUTPUT);
+    pinMode(green,OUTPUT);
+    pinMode(blue,OUTPUT);
 
     // TODO 6:
     // Print initialization message
+    Serial.println("RGB LED Control Initialized");
 }
 
 void loop() {
