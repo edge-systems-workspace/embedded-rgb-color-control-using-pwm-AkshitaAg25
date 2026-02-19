aadc277d8f04239a342ca6deef09933a60509374
AkshitaAg25 <akshita.agarwal_cs.aiml24@gla.ac.in>
2026-02-19 20:12:04 +0530

    final commit

--- src/main.cpp
+++ src/main.cpp
@@ -45,21 +45,27 @@ void loop() {
 
     // TODO 7:
     // Turn ON red (digital HIGH)
+    digitalWrite(red,HIGH);
 
     // TODO 8:
     // Turn OFF red
+    digitalWrite(red,LOW);
 
     // -------- ANALOG (PWM) MODE --------
 
     // TODO 9:
     // Set RED brightness using analogWrite()
+    analogWrite(red,128); // 50% brightness
 
     // TODO 10:
     // Set GREEN brightness using analogWrite()
+    analogWrite(green,64); // 25% brightness
 
     // TODO 11:
     // Set BLUE brightness using analogWrite()
+    analogWrite(blue,255); // 100% brightness
 
     // TODO 12:
     // Add delay for visible transition
+    delay(1000);
 }
